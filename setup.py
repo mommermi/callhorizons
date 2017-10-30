2bd68f7960e4ed4704b2491f2a75230526e1a1a3
Michael Mommert <michael.mommert@nau.edu>
2017-10-29 22:09:54 -0700

    minor modifications

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "CALLHORIZONS",
-    version = "1.0.7",
+    version = "1.0.8",
     author = "Michael Mommert",
     author_email = "michael.mommert@nau.edu",
     description = "CALLHORIZONS is a Python interface to access JPL HORIZONS ephemerides and orbital elements of Solar System bodies.",
